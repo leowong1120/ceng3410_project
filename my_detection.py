af170750f928c2d36694ba839322be41842b1d18
leowong1120 <49244227+leowong1120@users.noreply.github.com>
2021-04-27 13:02:43 +0800

    Update my_detection.py

--- my_detection.py
+++ my_detection.py
@@ -1,16 +1,17 @@
-import jetson.inference
-import jetson.utils
+def face:
+	import jetson.inference
+	import jetson.utils
 
-net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
-camera = jetson.utils.videoSource("csi://0")      # '/dev/video0' for V4L2
-display = jetson.utils.videoOutput("display://0") # 'my_video.mp4' for file
+	net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
+	camera = jetson.utils.videoSource("csi://0")      # '/dev/video0' for V4L2
+	display = jetson.utils.videoOutput("display://0") # 'my_video.mp4' for file
 
-while display.IsStreaming():
-	img = camera.Capture()
-	detections = net.Detect(img)
-	for detection in detections:
-		if (net.GetClassDesc(detection.ClassID) == "person"):
-			return
-	display.Render(img)
-	display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
+	while display.IsStreaming():
+		img = camera.Capture()
+		detections = net.Detect(img)
+		for detection in detections:
+			if (net.GetClassDesc(detection.ClassID) == "person"):
+				return
+		display.Render(img)
+		display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
 	
